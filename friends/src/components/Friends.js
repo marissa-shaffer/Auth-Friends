b43d8d6b66add19cdc872aab8d8b0c2f01f34aed
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-14 20:26:43 -0500

    Built Freinds component

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Friends = () => {
+    const [friend, setFriend] = useState([]);
+    const[update, setUpdate] = useState(false);
 
 
+    return (
+        <div>
+            <h1>Friends</h1>
+            <div>
+                {friend.map(item => {
+                    return(
+                        <div key={item.id}>
+                            <h2>Id: {item.id}</h2>
+                            <p>Name: {item.name}</p>
+                            <p>Age: {item.age}</p>
+                            <p>Email: {item.email}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
 
 export default Friends; 
